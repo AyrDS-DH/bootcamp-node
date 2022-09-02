2687b09342d3476b4d4e71097117acec1d02514c
AyrDS <ayrtonds97@gmail.com>
2022-09-02 11:43:48 -0300

    Clase asincronia

---
+++ NodeJS/Clase 7 - Asincronia/async.js
@@ -0,0 +1,62 @@
+const users = [
+   {
+      id: 1,
+      name: 'Ayrton',
+      apellido: 'Da Silva'
+   },
+   {
+      id: 2,
+      name: 'Ignacio',
+      apellido: 'Feito'
+   },
+   {
+      id: 3,
+      name: 'Rodrigo',
+      apellido: 'Lauga'
+   },
+];
+
+const movies = [
+   {
+      name: 'Titanic',
+      genre: 'Drama'
+   },
+   {
+      name: 'El señor de los anillos',
+      genre: 'Fantasía'
+   },
+   {
+      name: 'El exorcista',
+      genre: 'Terror'
+   }
+]
+
+const allUsers = () => {
+   return new Promise((resolve, reject) => {
+      if (true) {
+         setTimeout(() => {
+            resolve(users);
+         }, 2000);
+      } else {
+         reject('No se pueden obtener todos los usuarios');
+      }
+   });
+}
+
+const allMovies = () => {
+   return new Promise((resolve, reject) => {
+      if (false) {
+         setTimeout(() => {
+            resolve(movies);
+         }, 3000);
+      } else {
+         reject('No se pueden obtener todas las peliculas');
+      }
+   });
+}
+
+
+module.exports = {
+   allUsers,
+   allMovies
+}
---
+++ NodeJS/Clase 7 - Asincronia/index.js
@@ -0,0 +1,58 @@
+const { allUsers, allMovies } = require('./async');
+
+
+//Callbacks - ❌ Ya casi no lo van a ver asi
+function manejoDeUsuarios(id, res) {
+   User.findById(id, function (err, data) {
+      if (err) {
+         return res.err
+      } else {
+         Task.user(data.id, function (err, data) {
+            if (err) {
+               return res.err
+            } else {
+
+            }
+         })
+      }
+
+   });
+}
+
+//Promesas - then - catch ✅
+allUsers()
+   .then(data => console.log(data))
+   .catch(err => console.log(err))
+   .finally(() => {
+      console.log('Finally');
+   });
+
+
+
+//Async/await
+const handlerUsers = async () => {
+   console.log('cargando...');
+
+   try {
+      const users = await allUsers();
+
+      console.log(users);
+   } catch (msg) {
+      console.log(msg);
+   }
+}
+
+
+//Promise.all - Es lo más optimo ✅
+const promises = async () => {
+   console.log('cargando...');
+
+   try {
+      const data = await Promise.all([allUsers(), allMovies()]);
+
+      console.log(data);
+
+   } catch (msg) {
+      console.log(msg);
+   }
+}
