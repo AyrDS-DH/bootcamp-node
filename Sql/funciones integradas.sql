7001e74641ba0bd43e37d79000d91a524b353eaa
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-09-16 16:57:43 -0300

    clase sql funct and peticiones

---
+++ Sql/funciones integradas.sql
@@ -0,0 +1,19 @@
+// CONCAT
+
+SELECT CONCAT(first_name, " ", last_name) FROM actors
+
+SELECT CONCAT(first_name, " ", last_name) AS "Nombre y Apellido" FROM actors
+// A su vez podemos asignarle un ALIAS a la función CONCAT dado que devuelve una columna con la concatenación.
+
+
+// COALESCE
+SELECT title, COALESCE(rating, "Sin rating") FROM actors
+
+// DATEDIFF
+SELECT title, DATEDIFF(NOW(), created_at) FROM actors
+// devuelve la diferencia entre la fecha de hoy la fecha del created_at
+
+// EXTRACT
+SELECT title, EXTRACT(day, created_at) FROM actors
+// extrae una parte de una fecha en particular, podes utilizar month y year como parametros.
+
